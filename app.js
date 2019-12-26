1f72591043c84d020692d7b690cd9a6c5e90975b
Vidmantas Burba <vidmantasburba@Vidmantass-Air.lan>
2019-12-26 19:00:08 +0200

    before add to git

--- app.js
+++ app.js
@@ -2,28 +2,27 @@
 var express 				= require("express");
 var app						= express();
 var bodyParser 				= require("body-parser");
-var mongoose 				= require("mongoose");
-var flash					= require("connect-flash")
-var seedDB 					= require("./seeds");
-var passport 				= require("passport");
-var User 					= require("./models/user");
-var LocalStrategy 			= require("passport-local");
+var mongoose 								= require("mongoose");
+var flash										= require("connect-flash")
+var seedDB 									= require("./seeds");
+var passport 								= require("passport");
+var User 										= require("./models/user");
+var LocalStrategy 					= require("passport-local");
 var passportLocalMongoose 	= require("passport-local-mongoose");
-var methodOverride 			= require("method-override");
-var Campground 				= require("./models/campground");
-var Comment 				= require("./models/comment");
-
-var commentRoutes 			= require("./routes/comments");
-var campgroundRoutes 		= require("./routes/campgrounds");
-var indexRoutes 			= require("./routes/index");
-var expressSanitizer 		= require("express-sanitizer");
-
-
+var methodOverride 					= require("method-override");
+var Campground 							= require("./models/campground");
+var Comment 								= require("./models/comment");
+var commentRoutes 					= require("./routes/comments");
+var campgroundRoutes 				= require("./routes/campgrounds");
+var indexRoutes 						= require("./routes/index");
+var expressSanitizer 				= require("express-sanitizer");
 
 
 //var User = require("./models/user");
 //var request = require("request");
 //seedDB();
+
+//===============DATABASE CONFIG======================
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect("mongodb+srv://vidmantas:desrainis@cluster0-teyia.mongodb.net/test?retryWrites=true&w=majority", {
 	useNewUrlParser: true, 
@@ -34,7 +33,9 @@ mongoose.connect("mongodb+srv://vidmantas:desrainis@cluster0-teyia.mongodb.net/t
 }).catch(err => {
 	console.log('ERROR:', err.message);
 });
+//===============DATABASE CONFIG======================
 
+//===============PACKAGE CONFIG======================
 app.use (bodyParser.urlencoded({extended:true}));
 app.use (express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -42,6 +43,8 @@ app.use(flash());
 app.use(expressSanitizer());
 app.use(express.static("public")); //use this later for CSS
 app.set ("view engine", "ejs");
+//===============PACKAGE CONFIG======================
+
 //===============PASSPORT CONFIG======================
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
@@ -61,16 +64,10 @@ app.use(function (req, res, next){
 	next()
 });
 
-
-
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
-
 //===============PASSPORT CONFIG======================
-//IMPORTANT
-
 
 
 //IMPORTANT
