4f66e1690bf3ebb51caa9f6a9718664676841772
Vidmantas Burba <vidmantasburba@Vidmantass-Air.lan>
2020-03-14 12:29:41 +0200

    change path to /auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,56 +1,60 @@
 
 const express = require('express');
 
-const router 		    = express.Router();
+const router = express.Router();
 const passport 	= require('passport');
-const User 		  = require('../models/user');
+const User = require('../models/user');
 
 
 //= ==============AUTH ROUTES======================
 
 // show register form
-router.get('/register', (_req, res) => {
-    res.render('register', { page: 'register' });
-  });
-  
-  router.post('/register', (req, res) => {
-    const newUser = new User({ username: req.body.username });
-    if (req.body.adminCode === '123123') {
-      newUser.isAdmin = true;
+router.get('/register', (err, res) => {
+  if (err) {
+    console.log(err);
+    return res.render('register', { error: err.message });
+  }
+  res.render('register', { page: 'register' });
+});
+
+router.post('/register', (req, res) => {
+  const newUser = new User({ username: req.body.username });
+  if (req.body.adminCode === '123123') {
+    newUser.isAdmin = true;
+  }
+  eval(require('locus'));
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render('register', { error: err.message });
     }
-    eval(require('locus'));
-    User.register(newUser, req.body.password, (err, user) => {
-      if (err) {
-        console.log(err);
-        return res.render('register', { error: err.message });
-      }
-      passport.authenticate('local')(req, res, () => {
-        req.flash('success', `Welcome ${user.username}`);
-        res.redirect('/campgrounds');
-      });
+    passport.authenticate('local')(req, res, () => {
+      req.flash('success', `Welcome ${user.username}`);
+      res.redirect('/campgrounds');
     });
   });
-  
-  //= ==============AUTH ROUTES======================
-  
-  //= ===== Login Routes ========
-  
-  // show login form
-  router.get('/login', (_req, res) => {
-    res.render('login', { page: 'login' });
-  });
-  
-  router.post('/login', passport.authenticate('local', {
-    successRedirect: '/campgrounds',
-    failureRedirect: '/login',
-  }));
-  
-  //= ===== Logout Route ========
-  
-  router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Logged you out');
-    res.redirect('/');
-  });
+});
 
-  module.exports = router;
+//= ==============AUTH ROUTES======================
+
+//= ===== Login Routes ========
+
+// show login form
+router.get('/login', (_req, res) => {
+  res.render('login', { page: 'login' });
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/campgrounds',
+  failureRedirect: '/login',
+}));
+
+//= ===== Logout Route ========
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'Logged you out');
+  res.redirect('/');
+});
+
+module.exports = router;
